75c75c7d39321136cf8575f4a07dcf1612139c74
codegenius2 <lucifer231220@gmail.com>
2022-05-25 00:58:26 +0300

    Bottomsheet for Register

--- app/src/main/java/com/armutyus/ninova/MainActivity.kt
+++ app/src/main/java/com/armutyus/ninova/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -21,7 +22,9 @@ class MainActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navController = navHostFragment.navController
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
--- app/src/main/java/com/armutyus/ninova/ui/login/LoginActivity.kt
+++ app/src/main/java/com/armutyus/ninova/ui/login/LoginActivity.kt
@@ -4,8 +4,13 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.armutyus.ninova.MainActivity
+import com.armutyus.ninova.R
 import com.armutyus.ninova.databinding.ActivityLoginBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.android.material.button.MaterialButton
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
@@ -16,11 +21,27 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.register.setOnClickListener {
+            showRegisterDialog()
+        }
+
         binding.withoutRegister.setOnClickListener {
             startActivity(Intent(this,MainActivity::class.java))
         }
 
     }
 
+    private fun showRegisterDialog() {
+        val dialog = BottomSheetDialog(this)
+        dialog.setContentView(R.layout.register_user_bottom_sheet)
+        val signUpButton = dialog.findViewById<MaterialButton>(R.id.signUpButton)
+
+        signUpButton?.setOnClickListener {
+            //To-Do --> Save user data to firebase
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+
+        dialog.show()
+    }
 
 }
