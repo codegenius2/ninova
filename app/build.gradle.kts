84e9d95b421841ef49fa1d09cb1fb031f7d21800
codegenius2 <lucifer231220@gmail.com>
2023-11-25 16:35:45 +0300

    version updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.armutyus.ninova"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.1.0"
+        versionCode = 7
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
